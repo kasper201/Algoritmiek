073e52b6a48a2eb209c9fb56d646f8bb5796c198
Kasper <kasperjnssen@gmail.com>
2024-02-14 02:10:36 +0100

    Week2Deel2 af

--- Week2Deel2/TBS.cpp
+++ Week2Deel2/TBS.cpp
@@ -13,15 +13,23 @@ TBS::~TBS() {
 
 }
 
+void TBS::clear() {
+    for(int i = 0; i < index; i++)
+    {
+        rij[i] = 0;
+    }
+    index = 0;
+}
+
 void TBS::add(int value) {
-    array[index] = value;
+    rij[index] = value;
     index++;
 }
 
 void TBS::swap(int index1, int index2) {
-    int temp = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temp;
+    int temp = rij[index1];
+    rij[index1] = rij[index2];
+    rij[index2] = temp;
     std::cout << "New Swap" << std::endl;
     showAll();
 }
@@ -32,7 +40,7 @@ void TBS::selectionSort() {
     for(out = 0; out < index-1; out++) {
         min = out;
         for (in = out + 1; in < index; in++) {
-            if (array[in] > array[min]) {
+            if (rij[in] > rij[min]) {
                 min = in;
             }
         }
@@ -40,10 +48,25 @@ void TBS::selectionSort() {
     }
 }
 
+void TBS::insertionSort(int start) {
+    int in, out;
+    for(out = start + 1; out < index; out++) {
+        int temp = rij[out];
+        in = out;
+        while(in > 0 && rij[in-1] >= temp) {
+            rij[in] = rij[in-1];
+            --in;
+        }
+        rij[in] = temp;
+        std::cout << "New Swap" << std::endl;
+        showAll();
+    }
+}
+
 void TBS::showAll() const {
     for(int i = 0; i < 10; i++)
     {
-        std::cout << array[i] << " ";
+        std::cout << rij[i] << " ";
     }
     std::cout << std::endl;
 }
--- Week2Deel2/main.cpp
+++ Week2Deel2/main.cpp
@@ -10,6 +10,8 @@ void vraag1()
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(1, 100);
 
+    // clear array and add new random numbers
+    tbs.clear();
     for(int i = 0; i < 10; i++)
     {
         tbs.add(dis(gen));
@@ -19,21 +21,78 @@ void vraag1()
     tbs.selectionSort();
     std::cout << "Sorted" << std::endl;
     tbs.showAll();
-
 }
 
 void vraag2()
 {
+    TBS tbs;
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dis(1, 100);
 
+    // clear array and add new random numbers
+    tbs.clear();
+    for(int i = 0; i < 10; i++)
+    {
+        tbs.add(dis(gen));
+    }
+    tbs.showAll();
+
+    int start;
+    std::cout << "Enter start index(starting at 0): ";
+    std::cin >> start;
+    std::cout << "Sorting" << std::endl;
+    tbs.insertionSort(start);
+    std::cout << "Sorted" << std::endl;
+    tbs.showAll();
+}
+
+void testArray()
+{
+    TBS tbs;
+    std::cout << "\nTest array" << std::endl;
+
+    // set array to given test array
+    std::cout << "Clearing array" << std::endl;
+    tbs.clear();
+    tbs.add(7); tbs.add(3); tbs.add(8); tbs.add(1); tbs.add(2); tbs.add(5); tbs.add(4); tbs.add(6); tbs.add(9); tbs.add(0);
+    tbs.showAll();
+    std::cout << "Sorting using selection sort" << std::endl;
+    tbs.selectionSort();
+    std::cout << "Sorted" << std::endl;
+    tbs.showAll();
+
+    // set array to given test array
+    std::cout << "Clearing array" << std::endl;
+    tbs.clear();
+    tbs.add(7); tbs.add(3); tbs.add(8); tbs.add(1); tbs.add(2); tbs.add(5); tbs.add(4); tbs.add(6); tbs.add(9); tbs.add(0);
+    tbs.showAll();
+    std::cout << "Sorting using insertion sort" << std::endl;
+    int start;
+    std::cout << "Enter start index(starting at 0): ";
+    std::cin >> start;
+    tbs.insertionSort(start);
+    std::cout << "Sorted" << std::endl;
+    tbs.showAll();
 }
 
 int main() {
     /////////////////////////////////////////////////////////////////////////////////
     // Vraag 1 //
+    /*
+     * Het algoritme is O(n^2) omdat er nogsteeds twee loops zijn die over de array heen gaan voor comparions.
+     * Enkel de swap functie wordt nu O(N) omdat deze enkel een keer per paar wordt aangeroepen en dus max N keer.
+     */
     vraag1();
     /////////////////////////////////////////////////////////////////////////////////
     // Vraag 2 //
-
+    /*
+     * Het algoritme is O(N^2) omdat het algoritme nogsteeds twee loops heeft die over de array heen gaan en worst case all elementen van de array moeten worden vergeleken en geswapped na elke iteratie.
+     */
+    vraag2();
+    /////////////////////////////////////////////////////////////////////////////////
+    // Vragen met gegeven test array //
+    testArray();
     /////////////////////////////////////////////////////////////////////////////////
     return 0;
 }
