b35be70fe5ebd04cb47b90aaee66028927f81f59
Kasper <kasperjnssen@gmail.com>
2024-03-03 01:11:38 +0100

    It crashes due to a known error todo with the getting of the teams.

--- mogelijkGeenAlgo/Driver info/driverinfo.cpp
+++ mogelijkGeenAlgo/Driver info/driverinfo.cpp
@@ -5,6 +5,16 @@
 #include <fstream>
 #include <sstream>
 #include <vector>
+#include <curl/curl.h>
+#include <algorithm>
+
+std::string requestPath()
+{
+    std::string path;
+    std::cout << "Enter the path to the JSON file: ";
+    std::cin >> path;
+    return path;
+}
 
 driverinfo::driverinfo()
 {
@@ -15,9 +25,45 @@ driverinfo::~driverinfo()
 {
 }
 
+size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output)
+{
+    size_t totalSize = size * nmemb;
+    output->append((char*)contents, totalSize);
+    return totalSize;
+}
+
+std::string driverinfo::getRequest(const std::string &url) {
+    CURL* curl;
+    CURLcode res;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+
+    std::string response;
+
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+        res = curl_easy_perform(curl);
+
+        if(res != CURLE_OK) {
+            std::cerr << "Error: " << curl_easy_strerror(res) << std::endl;
+        }
+
+        curl_easy_cleanup(curl);
+    }
+
+    curl_global_cleanup();
+
+    return response;
+}
+
 int driverinfo::driver() {
     // Specify the path to your JSON file
     std::string filePath = "C:\\Users\\Kasper\\Downloads\\Agoritmiek\\.idea\\httpRequests\\2024-02-26T141655.200.json";
+
+    filePath = requestPath();
 
     // Read JSON data from file
     std::ifstream file(filePath);
@@ -55,11 +101,41 @@ int driverinfo::driver() {
         }
     }
 
+    for(int i = 0; i < familyNames.size(); i++) {
+        getTeam(familyNames[i]);
+    }
+
     // Print names and nationalities
     for (size_t i = 0; i < givenNames.size(); ++i) {
         fullNames.push_back(givenNames[i] + " " + familyNames[i]);
-        std::cout << i << " - Nationality: " << nationalities[i] << " full name: " << fullNames[i] << " Permanent number: " << permanentNumbers[i] << std::endl;
+        std::cout << i << " - Nationality: " << nationalities[i] << " full name: " << fullNames[i] << " Permanent number: " << permanentNumbers[i] << " Team: " << teams[familyNames[i]][i] << std::endl;
     }
 
     return 0;
+}
+
+void driverinfo::getTeam(std::string driver) {
+    std::replace(driver.begin(), driver.end(), ' ', '_');
+    std::string url = "http://ergast.com/api/f1/drivers/" + driver + "/constructors.json";
+    std::cout << "URL: " << url << std::endl;
+    std::string response = getRequest(url);
+    std::cout << response << std::endl;
+
+    std::istringstream stream(response);
+    while (stream) {
+        std::string token;
+        stream >> token;
+        if (token == "\"name\":") {
+            std::string team;
+            stream >> std::ws;
+            stream >> std::quoted(team);
+            teams[driver].push_back(team);
+            std::cout << "Team: " << team << std::endl;
+            std::cout << "Team: " << teams[driver].back() << std::endl;
+        }
+    }
+
+    for (size_t i = 0; i < teams[driver].size(); i++) {
+        std::cout << "Team: " << teams[driver][i] << std::endl;
+    };
 }
--- mogelijkGeenAlgo/Driver info/driverinfo.h
+++ mogelijkGeenAlgo/Driver info/driverinfo.h
@@ -5,12 +5,21 @@
 #ifndef WEEK2DEEL2_DRIVERINFO_H
 #define WEEK2DEEL2_DRIVERINFO_H
 
+#include <string>
+#include <map>
+#include <vector>
 
 class driverinfo {
 public:
     driverinfo();
     ~driverinfo();
     int driver();
+private:
+    std::string getRequest(const std::string &url);
+    void getTeam(std::string driver);
+
+private:
+    std::map<std::string, std::vector<std::string>> teams;
 };
 
 
