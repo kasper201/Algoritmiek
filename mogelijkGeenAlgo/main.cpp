31e754aaba95ead56f5c630d5ae4d2d344fcc363
Kasper <kasperjnssen@gmail.com>
2024-03-04 17:20:40 +0100

    Added ability to retrieve sprint race data

--- mogelijkGeenAlgo/main.cpp
+++ mogelijkGeenAlgo/main.cpp
@@ -1,11 +1,46 @@
 #include <iostream>
 #include "Driver info/driverinfo.h" // MN OGEN AAAAA WAAROM HEB IK HIER EEN SPATIE IN DE DIR GEDAAN??!?!?!?!
 #include "Circuit/CircuitInfo.h"
+#include "results/Results.h"
+
+#include <fstream>
+#include <sstream>
+
+int getAmountOfRaces(int year) {
+    std::string url = "https://ergast.com/api/f1/" + std::to_string(year) + ".json";
+    htmlRequest request;
+    std::string response = request.getRequest(url);
+    CorrectJson correctJson;
+    std::string correctedJson = correctJson.correctJson(response);
+    std::ofstream fileSave("tempAmountOfRaces.json", std::ios::trunc);
+    fileSave << correctedJson;
+    fileSave.close();
+    std::ifstream file("tempAmountOfRaces.json");
+    if (!file.is_open()) {
+        std::cerr << "Error opening file" << std::endl;
+    }
+    std::stringstream buffer;
+    buffer << file.rdbuf();
+    std::string data = buffer.str();
+    std::istringstream stream(data);
+    std::string token;
+    int amountOfRaces = 0;
+    while(stream) {
+        stream >> token;
+        if(token == "\"rounds\":") {
+            stream >> amountOfRaces;
+            break;
+        }
+    }
+    return amountOfRaces;
+}
 
 int main() {
     driverinfo driverinfo;
     CircuitInfo circuitInfo;
+    Results results;
     //driverinfo.driver();
-    circuitInfo.circuit();
+    //circuitInfo.circuit();
+    results.results(2023, 9);
     return 0;
 }
--- mogelijkGeenAlgo/results/Results.h
+++ mogelijkGeenAlgo/results/Results.h
@@ -7,14 +7,19 @@
 #include "htmlRequest.h"
 #include "CorrectJson.h"
 
+#include <vector>
+
 class Results {
 public:
     Results();
     virtual ~Results();
-    int results(int year);
+    int results(int year, int race);
+private:
+    int getSprintResults(int year, int race);
 private:
     htmlRequest request;
     CorrectJson correctJson;
+    std::vector<std::string> date, time, driver, position, points;
 };
 
 
